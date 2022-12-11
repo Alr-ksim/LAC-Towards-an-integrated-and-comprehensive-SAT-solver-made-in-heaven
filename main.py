a90bb412e0157560812f46d0144eec5c887a0ba8
Alr-ksim <3457265529@qq.com>
2022-12-11 21:54:56 +0800

    action-test5

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ from utils import read_cnf
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-i", "--input", type=str, default="examples/bmc-1.cnf"
+        "-i", "--input", type=str, default="examples/bmc-2.cnf"
+    )
+    parser.add_argument(
+        "-o", "--output", type=str, default="results/output.txt"
     )
 
     return parser.parse_args()
@@ -20,10 +23,14 @@ def main(args):
     # Create CDCL solver and solve it!
     res = cdcl(sentence, num_vars)
 
+    save_path = open(args.output, "w", encoding="utf-8")
+
     if res is None:
-        print("✘ No solution found")
+        save_path.write("✘ No solution found")
     else:
-        print(f"✔ Successfully found a solution: {res}")
+        save_path.write(f"✔ Successfully found a solution: {res}")
+
+    save_path.close()
 
 if __name__ == "__main__":
     args = parse_args()
